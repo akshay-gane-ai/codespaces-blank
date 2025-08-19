ceb25c1c05eb9e6fc6784ce5af6bf5cfd81d7a1a
akshay-gane-ai <akshay.gane.ai@gmail.com>
2025-08-19 18:34:47 +0000

    fixed pdf util issue

--- med_rag_bot/app/pdf_utils.py
+++ med_rag_bot/app/pdf_utils.py
@@ -1,19 +1,18 @@
 from pypdf import PdfReader
 
-def extract_text_from_pdf(pdf_path: str) -> str:
+def extract_text_from_pdf(file) -> str:
     """
     Extracts text from a PDF file.
 
     Args:
-        pdf_path (str): The path to the PDF file.
+        file :  PDF file.
         Example: "path/to/document.pdf"
 
     Returns:
         str: The extracted text from the PDF.
     """
-    with open(pdf_path, "rb") as file:
-        reader = PdfReader(file)
-        text = ""
-        for page in reader.pages:
+    reader = PdfReader(file)
+    text = ""
+    for page in reader.pages:
             text += page.extract_text() or ""
     return text
