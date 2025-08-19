275603f37e7737cf3c87563866c9255e940649f7
akshay-gane-ai <akshay.gane.ai@gmail.com>
2025-08-19 19:01:14 +0000

    moved file one level up

--- med_rag_bot/app/chat_utils.py
+++ med_rag_bot/app/chat_utils.py
@@ -9,8 +9,7 @@ def get_chat_model():
     """
     Create a chat model using the Euri AI API with specified parameters.
     """
-    config_path = os.path.join(os.path.dirname(__file__), "..", "config.json")
-    with open(config_path, "r") as config_file:
+    with open("config.json", "r") as config_file:
         config = json.load(config_file)
     return create_chat_model(
     api_key=EURI_API_KEY,
